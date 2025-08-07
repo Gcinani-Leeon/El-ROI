72afd3ecec7c4eb94d91ab7ddbc73cd45fca481d
Gcinani.Leeon <gcinani.leeon011@gmail.com>
2025-08-07 21:37:51 +0200

    INIT PUSH

---
+++ EL ROI WEBSITE/ex.js
@@ -0,0 +1,3 @@
+window.onload = function () {
+    alert("Welcome y'all!   Unfortunately this page is still under construction, please forgive us for the inconvinience. For further info, contact Gcinani Leeon Xulu (gcinani.leeon011@gmail.com/ 0638526007)");
+}
